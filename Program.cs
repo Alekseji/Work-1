ec59291b8c41cf0df8a75ea96a8157c0bc70ebd6
Alekseji <118532252+Alekseji@users.noreply.github.com>
2022-11-25 15:19:47 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -36,4 +36,18 @@ if (num3>max) max=num3;
 {
   Console.WriteLine($"max={max}");
 }*/
+/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
+/*Console.WriteLine("Imput number ");
+int num=Convert.ToInt32(Console.ReadLine());
+if (num % 2==0)
+{
+  Console.WriteLine("четное");
+}
+else
+{
+  Console.WriteLine("нечетное");
+}*/
 
