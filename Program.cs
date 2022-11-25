f5a0e8bfceaf23b581ae37148af9de2f6e5568e9
Alekseji <118532252+Alekseji@users.noreply.github.com>
2022-11-25 14:50:13 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -19,5 +19,21 @@ else
 {
   Console.WriteLine($"max{num1} is min{num2}");  
 }*/
-
+/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+/*Console.WriteLine("Imput number 1");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Imput number 2");
+int num2=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Imput number 3");
+int num3=Convert.ToInt32(Console.ReadLine());
+int max=num1;
+if (num1>max) max=num1;
+if (num2>max) max=num2;
+if (num3>max) max=num3;
+{
+  Console.WriteLine($"max={max}");
+}*/
 
