4c4c4a74c7eecdbba54721d886a64e1d78b0d0c5
Alekseji <118532252+Alekseji@users.noreply.github.com>
2022-11-27 14:49:08 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,3 +14,19 @@ int random = new Random().Next(100,1000);
 int newNumber = Secondnumbember(random);
 Console.WriteLine($"New version of {random} is {newNumber}");*/
 
+/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+/*void main (int num)
+{
+    if(num>100 && num<1000)
+    {
+        Console.WriteLine(num%10);
+    }
+    else
+    {
+       Console.WriteLine("Третьей цифры нет"); 
+    }
+}
+main(56);*/
